b57602b71e6ddcd37c656ecd2c503e248ab3f471
EtimeLEE <1015607255@qq.com>
2016-10-19 14:11:56 +0800

    my 2048

---
+++ js/support2048.js
@@ -0,0 +1,132 @@
+var documentWidth = window.screen.availWidth;
+var gridContainerWidth = 0.92 * documentWidth;
+var cellSlideLength = 0.18 * documentWidth;
+var cellSpace = 0.04 * documentWidth;
+
+
+
+
+
+function getPosTop(i,j){
+	return cellSpace + i*(cellSpace+cellSlideLength);
+}
+
+function getPosLeft(i,j){
+	return cellSpace + j*(cellSpace+cellSlideLength);
+}
+
+function getNumberBackgroundColor(number){
+	switch(number){
+		case 2:return "#eee4da";
+    	case 4:return "#ede0c8";
+    	case 8:return "#f2b179";
+    	case 16:return "#f59563";
+    	case 32:return "#f67c5f";
+    	case 64:return "#f65e3b";
+    	case 128:return "#edcf72";
+    	case 256:return "#edcc61";
+    	case 512:return "#9c0";
+    	case 1024:return "#33b5e5";
+    	case 2048:return "#09c";
+    	case 4096:return "#a6c";
+    	case 8192:return "#93c";
+
+	}
+	return "#000";
+}
+
+function getNumberColor(number){
+	if(number <= 4){
+		return "#776e65";
+	}
+	return "#fff";
+}
+
+function nospace(board){
+	for(var i = 0; i < 4; i++){
+		for(var j = 0; j < 4; j++){
+			if(board[i][j] === 0){
+				return false;
+			}
+		}	
+	}
+	return true;
+}
+
+function canMoveUp(board){
+	for(var i = 1; i < 4; i++){
+		for(var j = 0; j < 4; j++){
+			if(board[i][j] !== 0){
+				if((board[i-1][j] === 0) || (board[i-1][j] === board[i][j])){
+					return true;
+				}
+			}
+		}
+	}
+	return false;
+}
+
+
+function canMoveDown(board){
+	for(var i = 2; i >= 0; i--){
+		for(var j = 0; j < 4; j++){
+			if(board[i][j] !== 0){
+				if((board[i+1][j] === 0) || (board[i+1][j] === board[i][j])){
+					return true;
+				}
+			}
+		}
+	}
+	return false;
+}
+
+function canMoveLeft(board){
+	for(var i = 0; i < 4; i++){
+		for(var j = 1; j < 4; j++){
+			if(board[i][j] !== 0){
+				if((board[i][j-1] === 0) || (board[i][j-1] === board[i][j])){
+					return true;
+				}
+			}
+		}
+	}
+	return false;
+}
+
+function canMoveRight(board){
+	for(var i = 0; i < 4; i++){
+		for(var j = 2; j >= 0; j--){
+			if(board[i][j] !== 0){
+				if((board[i][j+1] === 0) || (board[i][j+1] === board[i][j])){
+					return true;
+				}
+			}
+		}
+	}
+	return false;
+}
+
+function noBlockHorizontal(row,col1,col2,board){
+	for(var i = col1 + 1; i < col2; i++){
+		if(board[row][i] !== 0){
+			return false;
+		}
+	}
+	return true;
+}
+
+function noBlockVertical(col,row1,row2,board){
+	for(var i = row1 + 1; i < row2; i++){
+		if(board[i][col] !== 0){
+			return false;
+		}
+	}
+	return true;
+}
+
+function nomove(){
+	if(canMoveUp(board)||canMoveDown(board)||canMoveLeft(board)||canMoveRight(board)){
+		return false;
+	}
+	return true;
+}
